2da9468f01144c4033496c50a7e73e119cfa1641
antoine-gajan <86797909+antoine-gajan@users.noreply.github.com>
2022-01-21 18:19:54 +0100

    Update pendu.c

--- Pendu/pendu.c
+++ Pendu/pendu.c
@@ -15,17 +15,17 @@ int main(int argc, char *argv[])
     //Affichage du menu
     menu(&nb_erreurs_restant);
     //Demande du mot secret de moins de 30 lettres
-    printf("=== JOUEUR 1 ===\n\nMot secret (écrit en majuscules) de moins de 30 mots à faire deviner à votre partenaire : ");
+    printf("=== JOUEUR 1 ===\n\nMot secret (Ã©crit en majuscules) de moins de 30 mots Ã  faire deviner Ã  votre partenaire : ");
     char motSecret[30] = "";
     scanf("%s", motSecret);
-    //On vide le buffer par sécurité
+    //On vide le buffer par sÃ©curitÃ©
     viderBuffer();
 
-    //Tableau de booléen avec allocation de mémoire dynamique
+    //Tableau de boolÃ©en avec allocation de mÃ©moire dynamique
     int *tableau_booleen = malloc(taille_mot(motSecret) * sizeof(int));
     InitialiserTableau(tableau_booleen, taille_mot(motSecret));
 
-    printf("\n\n=== JOUEUR 2 ===\n\nVous devez découvrir un mot de %d lettres.\n\nVous avez le droit à %d erreurs maximum.\n", taille_mot(motSecret), nb_erreurs_restant);
+    printf("\n\n=== JOUEUR 2 ===\n\nVous devez dÃ©couvrir un mot de %d lettres.\n\nVous avez le droit Ã  %d erreurs maximum.\n", taille_mot(motSecret), nb_erreurs_restant);
     //boucle principale
     while (!gagne && nb_erreurs_restant > 0)
     {
@@ -37,26 +37,27 @@ int main(int argc, char *argv[])
     }
     if (gagne)
     {
-        printf("\nBravo ! Vous avez gagné ! Vous avez découvert le mot : %s", motSecret);
+        printf("\nBravo ! Vous avez gagnÃ© ! Vous avez dÃ©couvert le mot : %s", motSecret);
     }
     else
     {
-        printf("\nDommage ! Le mot à découvrir était : %s", motSecret);
+        printf("\nDommage ! Le mot Ã  dÃ©couvrir Ã©tait : %s", motSecret);
     }
+    free(tableau_booleen);
     return 0;
 }
 
 
 void menu(int *nb_erreurs_restant){
-    //Demande du choix de la difficulté
+    //Demande du choix de la difficultÃ©
     int reponse = 0;
     do{
-        printf("Choisir un niveau de difficulté :\n1. Facile (10 erreurs autorisées)\n2. Moyen (8 erreurs autorisées)\n3. Difficile (5 erreurs autorisées)\n\nVotre choix :");
+        printf("Choisir un niveau de difficultÃ© :\n1. Facile (10 erreurs autorisÃ©es)\n2. Moyen (8 erreurs autorisÃ©es)\n3. Difficile (5 erreurs autorisÃ©es)\n\nVotre choix :");
         scanf("%d", &reponse);
         viderBuffer();
     }while(reponse != 1 && reponse != 2 && reponse !=3);
 
-    //Changement de la difficulté du jeu
+    //Changement de la difficultÃ© du jeu
     switch(reponse){
     case 1:
         *nb_erreurs_restant = 10;
@@ -73,7 +74,7 @@ void menu(int *nb_erreurs_restant){
 int taille_mot(char mot[])
 {
     int taille = 0, i = 0;
-    //tant qu'on n'est pas à la fin du mot, on incrémente la longueur du mot
+    //tant qu'on n'est pas Ã  la fin du mot, on incrÃ©mente la longueur du mot
     for (i = 0; mot[i] != '\0'; i++)
     {
         taille++;
@@ -87,34 +88,34 @@ char LireUneLettre()
     //Initialisation des variables
     char caractere = 0;
     char chaine_rentree_utilisateur = 0;
-    //Saisie d'un caractère par l'utilisateur
+    //Saisie d'un caractÃ¨re par l'utilisateur
     printf("Entrez une lettre : ");
     caractere = fgetc(stdin);
     //On le met en majuscule
     caractere = toupper(caractere);
     //On vide le buffer
     viderBuffer();
-    //On retourne le caractère
+    //On retourne le caractÃ¨re
     return caractere;
 }
 
 void InitialiserTableau(int tableau[], int taille)
 {
     int i = 0;
-    //Toutes les valeurs mises à 0
+    //Toutes les valeurs mises Ã  0
     for (i = 0; i < taille; i++)
     {
         tableau[i] = 0;
     }
 }
 
-//Fonction pour mettre à jour le mot à trouver si la lettre donnée est dans le mot
+//Fonction pour mettre Ã  jour le mot Ã  trouver si la lettre donnÃ©e est dans le mot
 int LettreCorrecte(char lettre, char mot[], int tableau[], int *nb_erreurs_restant)
 {
     //Si la lettre est bien dans le mot
     if (strchr(mot, lettre) != NULL)
     {
-        //On modifie le tableau de booléen
+        //On modifie le tableau de boolÃ©en
         int i = 0;
         for (i = 0; i < taille_mot(mot); i++)
         {
@@ -126,12 +127,12 @@ int LettreCorrecte(char lettre, char mot[], int tableau[], int *nb_erreurs_resta
         printf("La lettre %c est dans le mot secret ! \n", lettre);
         return 1;
     }
-    //Si une erreur a été rencontrée, on enlève une erreur possible et on renvoie 0
+    //Si une erreur a Ã©tÃ© rencontrÃ©e, on enlÃ¨ve une erreur possible et on renvoie 0
     else
     {
         printf("Dommage ! La lettre %c n'est pas dans le mot secret... \n", lettre);
         *nb_erreurs_restant = *nb_erreurs_restant - 1;
-        printf("Nombre d'erreurs restant autorisé : %d\n", *nb_erreurs_restant);
+        printf("Nombre d'erreurs restant autorisÃ© : %d\n", *nb_erreurs_restant);
         return 0;
     }
 }
@@ -146,13 +147,13 @@ void viderBuffer()
     }
 }
 
-//Fonction pour afficher le mot avec des étoiles
+//Fonction pour afficher le mot avec des Ã©toiles
 void AfficheRestant(char motSecret[], int tableau[])
 {
     //Taille du mot secret
     int taille = taille_mot(motSecret);
     int i = 0;
-    printf("Mot à découvrir : ");
+    printf("Mot Ã  dÃ©couvrir : ");
     for (i = 0; i < taille_mot(motSecret); i++)
     {
         if (tableau[i] == 1)
@@ -167,7 +168,7 @@ void AfficheRestant(char motSecret[], int tableau[])
     printf("\n\n");
 }
 
-//Fonction qui met à jour le booléen gagner
+//Fonction qui met Ã  jour le boolÃ©en gagner
 void AGagner(int tableau[], int taille_mot_secret, int *gagne)
 {
     int i = 0;
